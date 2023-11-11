30988d4e3067cc3d7e69818b7db4da18e34ee55b
IwonskiI <ricky2101@naver.com>
2023-11-11 15:11:22 +0900

    Carousel done

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,2 +1,7 @@
-const images = {};
+const images = {
+    logo_b: '/assets/images/logo-b.svg',
+    Rarrow: '/assets/images/arrow_right.png',
+    Larrow: '/assets/images/arrow_left.png',
+
+};
 export default images;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Carousel from './components/BannerCarousel';
+import './styles/home.styles.css'
 
 type Props = {};
 
 const Home = (props: Props) => {
-	return <div className='home'></div>;
+	const images = [
+		"https://picsum.photos/1200/600?random=1",
+		"https://picsum.photos/1200/600?random=2",
+		"https://picsum.photos/1200/600?random=3"
+	  ];
+	  
+	return <div className='home'>
+	  <Carousel images={images} />
+	</div>;
 };
 
 export default Home;
